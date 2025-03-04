cd4ab20d0afb5323b36f354e986b70a88594a7f6
Felipe Munevar N <felipemunevarn@gmail.com>
2025-03-04 09:10:47 -0300

    by-passing spring security login page

--- gym/src/main/java/com/epam/gym/controller/Test.java
+++ gym/src/main/java/com/epam/gym/controller/Test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 public class Test {
-    
+
     @GetMapping("/v1")
     public String getTrainee() {
         return "This is the first test";
