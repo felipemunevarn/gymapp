f9ab00df45c89e3b1e99e8da7c155e37dc2f14e3
Felipe Munevar N <felipemunevarn@gmail.com>
2025-03-04 08:56:18 -0300

    initial commit

---
+++ gym/src/main/java/com/epam/gym/controller/Test.java
@@ -0,0 +1,15 @@
+package com.epam.gym.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class Test {
+    
+    @GetMapping("/v1")
+    public String getTrainee() {
+        return "This is the first test";
+    }
+}
